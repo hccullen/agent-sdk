2b52d2341de6f5894088f9a7cd9b6b91251864b5
Henrik Cullen <hccullen@users.noreply.github.com>
2026-09-11 18:30:26 +0200

    feat: add collectCitations() and toMarkdown() for inline citations (#20)

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -36,8 +36,8 @@ export type { WorkflowDefinition, WorkflowNode, CompiledGraph, AgentCallConfig, 
 
 export { CortiError, CortiTimeoutError, CortiSDKError, CortiSDKErrorCodes } from "./errors.js";
 
-export { parseSSEStream, parseA2AStream, makeAbortController, collectText, StreamCollector } from "./streaming.js";
-export type { SSEEvent, AbortOptions, StreamTextChunk } from "./streaming.js";
+export { parseSSEStream, parseA2AStream, makeAbortController, collectText, StreamCollector, collectCitations, toMarkdown } from "./streaming.js";
+export type { SSEEvent, AbortOptions, StreamTextChunk, Citation, StreamTextWithCitations } from "./streaming.js";
 
 export type {
   AgentID,
